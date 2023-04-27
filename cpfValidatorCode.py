4d83ecd45c4ac6e4de8f862ceda1c7a0571024ce
unknown <davi.cesarbt@gmail.com>
2023-04-27 14:03:54 -0300

    for structure made: multiplying the itens by the position reversed

--- cpfValidatorCode.py
+++ cpfValidatorCode.py
@@ -1,14 +1,19 @@
 cpf = input("Enter your CPF (put a space between each number): ")
 
 arrayCpf = cpf.split(" ")
-print(arrayCpf)
 
 lenCpfNum = len(arrayCpf)
 firt9digitsOfCpf = lenCpfNum - 2
 
-i = 10
+i = 11
+p = 0
+sumMultiplications = 0 # initializing the variable to store the sum of all multiplications
 
 for i in range(firt9digitsOfCpf):
     i -= 1
-    cpfItem = arrayCpf[i]
-    
+    cpfItem = int(arrayCpf[p]) # converting the item to an integer before multiplying
+    multiplication = cpfItem * i
+    p += 1
+    sumMultiplications += multiplication # adding the result of each multiplication to the sum
+
+print("Sum of multiplications:", sumMultiplications) 
