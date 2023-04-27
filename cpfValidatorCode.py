0014697317f613efe0f4b042a75c46d5f54ff245
unknown <davi.cesarbt@gmail.com>
2023-04-27 14:12:01 -0300

    variable name changed

--- cpfValidatorCode.py
+++ cpfValidatorCode.py
@@ -6,14 +6,14 @@ lenCpfNum = len(arrayCpf)
 firt9digitsOfCpf = lenCpfNum - 2
 
 i = 11
-p = 0
+position = 0
 sumMultiplications = 0 # initializing the variable to store the sum of all multiplications
 
 for i in range(firt9digitsOfCpf):
     i -= 1
-    cpfItem = int(arrayCpf[p]) # converting the item to an integer before multiplying
+    cpfItem = int(arrayCpf[position]) # converting the item to an integer before multiplying
     multiplication = cpfItem * i
-    p += 1
+    position += 1
     sumMultiplications += multiplication # adding the result of each multiplication to the sum
 
 print("Sum of multiplications:", sumMultiplications) 
