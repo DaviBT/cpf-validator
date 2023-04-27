a810f525f03250e2b34bac4d7d393d742397c62e
unknown <davi.cesarbt@gmail.com>
2023-04-27 15:52:54 -0300

    second verification made: for loop

--- cpfValidatorCode.py
+++ cpfValidatorCode.py
@@ -24,8 +24,24 @@ if restOf < 2:
 else:
     verifyingDigit = 11 - restOf
 
-b = 0
-while b <= 2:
-    arrayCpf.pop()
+secondVerificationCpfArray = arrayCpf
 
-arrayCpf.append(verifyingDigit)
+k = 10
+b = 0
+while b < 2:
+    secondVerificationCpfArray.pop(k)
+    b += 1
+    k -= 1
+
+factor = 11
+sumMultiplications2 = 0
+position2 = 0
+secondVerificationCpfArray.append(verifyingDigit)
+for i in range(10):
+    cpfItem2 = secondVerificationCpfArray[position2] 
+    cpfItem2 = int(cpfItem2) # converting the item to an integer before multiplying
+    multiplication2 = cpfItem2 * factor
+    sumMultiplications2 += multiplication2 # adding the result of each multiplication to the sum
+    position2 += 1
+    factor -= 1
+print(sumMultiplications2)
